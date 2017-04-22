083211026f77e6a2c45078d7a4816151f51df3f8
bryanhmiller <bryankalos@hotmail.com>
2017-04-21 20:27:26 -0500

    Continued working on xhr with no success.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,40 +1,53 @@
 $(document).ready(function(){
 
-	const loadContestant1 = () => {
-		return new Promise((resolve, reject) => {
-			$.ajax("https://teamtreehouse.com/krissycaron.json") 
-			.done((data1) => resolve(data1.humans))
-			.fail((error) => reject(error));
-		}); 
-	};
+	let myTeamTreeHouse = [];
 
-	const loadContestant2 = () => {
-		return new Promise((resolve, reject) => {
-			$.ajax("https://teamtreehouse.com/krissycaron.json") 
-			.done((data1) => resolve(data1.humans))
-			.fail((error) => reject(error));
-		}); 
-	};
+	// const loadContestant1 = () => {
+	// 	return new Promise((resolve, reject) => {
+	// 		$.ajax("https://teamtreehouse.com/krissycaron.json") 
+	// 		.done((data1) => resolve(data1.myTeamTreeHouse))
+	// 		.fail((error) => reject(error));
+	// 	}); 
+	// };
 
-	Promise.all([loadContestant1(), loadContestant2()])
-	.then((result) => {
-		result.forEach((xhrResult) => {
-			xhrResult.forEach((animal) => {
-				myAnimals.push(animal);
-			});
-		});
-		for (let i = 0; i < myHumans.length; i++) {
-			for (let j = 0; j < myAnimals.length; j++) {
-				if (checkForTypeMatch(myHumans[i], myAnimals[j]) && checkForKidFriendly(myHumans[i], myAnimals[j])){
-					myHumans[i].matches.push(myAnimals[j]);
-				}
-			}
-		}
-		writeToDOM(myHumans);
-	})
-	.catch(function(errors){
-		console.log(errors);
-	});
+	var constestantOneJSON = function(){
+        return new Promise(function(resolve, reject){
+            $.ajax("https://teamtreehouse.com/krissycaron.json").done(function(data1){
+                resolve(data1.myTeamTreeHouse);
+            }).fail(function(error1){
+                reject(error1);
+            });
+        });
+    };
+
+	// const loadContestant2 = () => {
+	// 	return new Promise((resolve, reject) => {
+	// 		$.ajax("https://teamtreehouse.com/krissycaron.json") 
+	// 		.done((data1) => resolve(data1.humans))
+	// 		.fail((error) => reject(error));
+	// 	}); 
+	// };
+
+    constestantOneJSON().then(function(xhrData){
+	    console.log("xhrData", xhrData);
+	    myTeamTreeHouse = xhrData;
+	    console.log("myTeamTreeHouse", myTeamTreeHouse);
+	});    
+
+
+	// Promise.all([loadContestant1()])// ,loadContestant2()
+	// .then((result) => {
+	// 	result.forEach((xhrResult) => {
+	// 		xhrResult.forEach((teamtreehouse) => {
+	// 			myTeamTreeHouse.push(teamtreehouse);
+	// 		});
+	// 	});
+	// 	// writeToDOM(myHumans);
+	// 	console.log(myTeamTreeHouse);
+	// })
+	// .catch(function(errors){
+	// 	console.log(errors);
+	// });
 
 
 });

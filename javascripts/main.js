d9ce754164113e96484646ae47ddae4ea8d525a9
bryanhmiller <bryankalos@hotmail.com>
2017-04-23 19:38:46 -0500

    Got ajax working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
-	let myTeamTreeHouse = [];
+	let contestantOne = [];
+	let contestantTwo = [];
+	let domString= "";
 
-	// const loadContestant1 = () => {
-	// 	return new Promise((resolve, reject) => {
-	// 		$.ajax("https://teamtreehouse.com/krissycaron.json") 
-	// 		.done((data1) => resolve(data1.myTeamTreeHouse))
-	// 		.fail((error) => reject(error));
-	// 	}); 
-	// };
+	const writeToDOM = () => {
+		domString += `<img class="img-circle" src="${contestantOne.gravatar_url}"/>`;
+		$("#cage").append(domString);
+	}:
+
     const loadContestantOne = () => {
         return new Promise((resolve, reject) => {
             $.ajax("https://teamtreehouse.com/bryanmiller5.json")
-            .done((data1) => resolve(data1.bryanmiller5))
+            .done((data1) => resolve(data1))
             .fail((error1) => reject(error1));
         });
     };
@@ -20,87 +20,23 @@ $(document).ready(function(){
     const loadContestantTwo = () => {
         return new Promise((resolve, reject) => {
             $.ajax("https://teamtreehouse.com/krissycaron.json")
-            .done((data2) => resolve(data2.krissycaron))
+            .done((data2) => resolve(data2))
             .fail((error2) => reject(error2));
         });
     };
 
-//     var firstDinosaurJSON = function(){
-//     	return new Promise(function(resolve, reject){
-//     		$.ajax("./db/dinosaurs1.json").done(function(data1){
-//     			resolve(data1.dinosaurs1);
-// 	    	}).fail(function(error1){
-// 	    		reject(error1);
-// 	    	})
-//     	})
-// 	};
 
+	loadContestantOne().then((result) => {
+		contestantOne = result;
+		console.log("contestantOne", contestantOne);
 
-// firstDinosaurJSON().then(function(jsonData1){
-// 	console.log(jsonData1)
-// 	dinosaurs = jsonData1
-// 	writeDOM();	
-// }).catch(function(jsonDataFail1){
-// 	console.log(jsonDataFail1);
-// });
-	// var constestantOneJSON = function(){
- //        return new Promise(function(resolve, reject){
- //            $.ajax("https://teamtreehouse.com/krissycaron.json")
- //            .done(function(data1){
- //                resolve(data1.myTeamTreeHouse);
- //            }).fail(function(error1){
- //                reject(error1);
- //            });
- //        });
- //    };
-
-	// const loadContestant2 = () => {
-	// 	return new Promise((resolve, reject) => {
-	// 		$.ajax("https://teamtreehouse.com/krissycaron.json") 
-	// 		.done((data1) => resolve(data1.humans))
-	// 		.fail((error) => reject(error));
-	// 	}); 
-	// }
-		// .then(function(result){
-		// 	result.forEach(function(xhrResult){
-		// 		xhrResult.forEach(function(animal){
-		// 			myAnimals.push(animal);
-
- //    loadContestantOne().then(function(jsonData1){
-	//     jsonData1.forEach(result) => {
-	//     	myTeamTreeHouse.push(result);
-	//     }
-	//     console.log("myTeamTreeHouse", myTeamTreeHouse);
-	// });    
-	// Promise.all([loadContestantOne(), loadContestantTwo()])
-	// .then(function(result){
-	// 	console.log("result", result);
-	// 	result.forEach(function(xhrResult){
-	// 		console.log("xhrResult", xhrResult);
-	// 		xhrResult.forEach(function(data){
-	// 			contestanOne.push(data);
-	// 			console.log("contestanOne", contestanOne);
-	// 		});
-	// 	});
-	// })
-	// .catch(function(errors){
-	// 	console.log(errors);
-	// });
-
-
-	Promise.all([loadContestantOne() ,loadContestantTwo()])
-	.then((result) => {
-		result.forEach((xhrResult) => {
-			xhrResult.forEach((teamtreehouse) => {
-				myTeamTreeHouse.push(teamtreehouse);
-			});
-		});
-		// writeToDOM(myHumans);
-		console.log(myTeamTreeHouse);
-	})
-	.catch(function(errors){
-		console.log(errors);
 	});
+
+	loadContestantTwo().then((result) => {
+		contestantTwo = result;
+		console.log("contestantTwo", contestantTwo.badges.name);
+	});
+
 
 
 });

d3cffc76ac9c5f2327a775cd499c76b6a3103ea2
bryanhmiller <bryankalos@hotmail.com>
2017-04-24 18:17:54 -0500

    Finally got data in the console

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,39 @@
 $(document).ready(function(){
 
-	let contestantOne = [];
-	let contestantTwo = [];
 	let domString= "";
+	// let dataStringTwo = "https://teamtreehouse.com/" + playerTwo + ".json";
+	$("#start-match").click(() => {
+		let playerOne = $("#box-one").val();
+		let playerTwo = $("#box-two").val();
+		startCageMatch(playerOne);
+	});
+
+	const startCageMatch = (playerOne) => {
+	    Promise.all([loadContestantOne(playerOne), loadContestantTwo()])
+	        .then(function(resultz){
+				let contestantOne = resultz[0];
+				console.log("contestantOne.name",contestantOne);
+	            let contestantTwo = resultz[1];
+	            resultz.forEach(function(ajaxCalls){
+	                ajaxCalls.forEach(function(dataStringOne){
+	                    contestantOne.push(dataStringOne);
+	                    console.log("contestantOne", contestantOne);
+	                });
+	            });
+	            writeDOM();
+	        });
+
+	};
 
 	const writeToDOM = () => {
-		domString += `<img class="img-circle" src="${contestantOne.gravatar_url}"/>`;
 		$("#cage").append(domString);
-	}:
+	};
 
-    const loadContestantOne = () => {
+    const loadContestantOne = (playerOne) => {
         return new Promise((resolve, reject) => {
-            $.ajax("https://teamtreehouse.com/bryanmiller5.json")
+		let dataStringOne = `https://teamtreehouse.com/${playerOne}.json`;
+		console.log(playerOne);
+            $.ajax(dataStringOne)
             .done((data1) => resolve(data1))
             .fail((error1) => reject(error1));
         });
@@ -25,17 +47,18 @@ $(document).ready(function(){
         });
     };
 
+    
+ //    Promise.all
+	// loadContestantOne().then((result) => {
+	// 	contestantOne = result;
+	// 	console.log("contestantOne", contestantOne);
 
-	loadContestantOne().then((result) => {
-		contestantOne = result;
-		console.log("contestantOne", contestantOne);
+	// });
 
-	});
-
-	loadContestantTwo().then((result) => {
-		contestantTwo = result;
-		console.log("contestantTwo", contestantTwo.badges.name);
-	});
+	// loadContestantTwo().then((result) => {
+	// 	contestantTwo = result;
+	// 	console.log("contestantTwo", contestantTwo);
+	// });
 
 
 
